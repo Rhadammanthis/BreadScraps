b1dd4e8c1a63a49328de39edc804a8e3c5dfff78
Hugo Medina <rhadammanthis@gmail.com>
2017-04-25 09:36:24 -0500

    Functions in server are now commented

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ var router = express.Router();
 //Auxiliar variables to process Search request
 var spotifyToken;
 var tokenExpired = false;
-// var analizeWholeSet;
+var analizeWholeSet;
 var factorNoLyrics;
 var results;
 var analyseSize;
@@ -35,6 +35,8 @@ var client_secret = '987de9323c0140cbbc2005bd29d73d54'; // Your secret
 router.post('/auth', (req, res) => {
 
     var code = req.body.code
+
+    console.log(code)
 
     var authOptions = {
         url: 'https://accounts.spotify.com/api/token',
@@ -55,6 +57,9 @@ router.post('/auth', (req, res) => {
             // use the access token to access the Spotify Web API
             var token = body.access_token;
             res.set('Access-Control-Allow-Origin', '*').status(200).json(body)
+        }
+        else{
+            res.set('Access-Control-Allow-Origin', '*').send(response)
         }
     });
 })
@@ -88,6 +93,8 @@ router.post('/getSaddestSongs', (req, res) => {
 
     console.time("call");
     console.time("complete");
+
+    //extracts the Spotify token, artist id and artist name from the request body
     spotifyToken = req.body.spotifyToken
     artist = req.body.artistName
     var artistId = req.body.artistId
@@ -101,7 +108,7 @@ router.post('/getSaddestSongs', (req, res) => {
     if (!artistId || artistId === "" || artistId === " ")
         res.set('Access-Control-Allow-Origin', '*').status(400).json({ error: { status: 400, message: 'Body should have an \'artist\' property' } })
 
-    //Checks if the request specified the result array's size. It's 10 by default
+    //Checks if additional parameters where included in the URL
     results = req.body.results ? req.body.results : 10
     factorNoLyrics = req.body.factor_no_lyrics ? (req.body.factor_no_lyrics == 'true') : false
     analyseSize = req.body.analyse_set_size ? req.body.analyse_set_size : 15
@@ -109,6 +116,7 @@ router.post('/getSaddestSongs', (req, res) => {
 
     console.log('Factor no lyrics', factorNoLyrics)
 
+    //Prepares the request to get all the artist's albums
     var options = {
         method: 'GET',
         url: 'https://api.spotify.com/v1/artists/' + artistId + '/albums',
@@ -122,13 +130,17 @@ router.post('/getSaddestSongs', (req, res) => {
 
         console.time("getSongs");
 
+        //Async call to get all the songs from every album
         async.map(albums, getAlbums, (asyncError, asyncResults) => {
             if (asyncError) {
                 res.set('Access-Control-Allow-Origin', '*').status(asyncError.error.status).json(asyncError);
             } else {
                 console.log('Total albums: ', asyncResults.length);
+                //Array to gold all retrieved songs
                 var songs = []
                 console.timeEnd("getSongs");
+
+                //Extracts all the songs from every album
                 asyncResults.forEach((album) => {
                     if (album != null)
                         album.songs.forEach((song) => {
@@ -138,26 +150,14 @@ router.post('/getSaddestSongs', (req, res) => {
                         })
                 })
 
-                // console.log('Artist:', artist)
-                // console.log('Total songs: ', songs.length);
-                // console.log('Total non empty songs: ', songs.filter((song) => {
-                //     if (song.hasOwnProperty('valence') && song.valence > 0)
-                //         return true
-                //     return false
-                // }).length);
-                // console.log('Total real songs: ', _.uniqBy(songs.filter((song) => {
-                //     if (song.hasOwnProperty('valence') && song.valence > 0)
-                //         return true
-                //     return false
-                // }), 'name').length);
-
                 console.time("filter");
 
-                var saddestSongs = _.uniqBy(songs.filter((song) => {
+                //Discards all songs that have a 'valence' property and that are not remixes or live performances
+                var bestCandidates = _.uniqBy(songs.filter((song) => {
                     if (song.hasOwnProperty('valence') && song.valence > 0 && !(song.name.includes("Rmx") || song.name.includes("RMX") || song.name.includes("Remix") || song.name.includes("REMIX") || song.name.includes("Live") || song.name.includes("Reprise")))
                         return true
                     return false
-                }), 'name').sort((a, b) => {
+                }), 'name').sort((a, b) => { //Sort the array from lowest to highest valence 
                     return a.valence - b.valence;
                 })
 
@@ -165,31 +165,38 @@ router.post('/getSaddestSongs', (req, res) => {
 
                 console.timeEnd("call");
 
+                //If true, the array is reduced to the specified size
                 if(analizeWholeSet === false)
-                    saddestSongs = saddestSongs.slice(0, analyseSize);
+                    bestCandidates = bestCandidates.slice(0, analyseSize);
 
-                console.log('Array size: ', saddestSongs.length)
+                console.log('Total Songs: ', bestCandidates.length)
 
-                async.map(saddestSongs, getSaddestSongs, (qError, qResult) => {
+                //Async call to get the saddest songs from the search size
+                async.map(bestCandidates, getSaddestSongs, (qError, qResult) => {
                     if (qError)
-                        res.json.qResult
+                        res.send(qError)
                     else {
+                        console.log('Total valid songs: ', qResult.filter((song) => {
+                            if (song && song != null)
+                                return true
+                            return false
+                        }).length)
                         console.timeEnd("complete");
+
+                        //Filters all the empty objects out, arranges the items from highest
+                        //to lowest Gloom Index and slices the array at the specified size 
                         res.set('Access-Control-Allow-Origin', '*').status(200).json(qResult.filter((song) => {
                             if (song && song != null)
                                 return true
                             return false
                         }).sort((a, b) => {
                             return b.gloomIndex - a.gloomIndex;
-                        }).slice(0, results > saddestSongs.length ? saddestSongs.length : results));
+                        }).slice(0, results > bestCandidates.length ? bestCandidates.length : results));
                     }
                 })
-
             }
         });
-
     });
-    // });
 })
 
 var getAlbums = (album, callback) => {
@@ -198,6 +205,7 @@ var getAlbums = (album, callback) => {
     rAlbum.title = album.name;
     rAlbum.songs = []
 
+    //Prepares the request to get all the songs from an album
     var options = {
         method: 'GET',
         url: 'https://api.spotify.com/v1/albums/' + album.id
@@ -205,20 +213,20 @@ var getAlbums = (album, callback) => {
 
     request(options, (error, response, body) => {
         if (error) return callback(error);
-        // console.log('===> 3')
 
-        // console.log('========>' + album.name)
-
+        //Saves the album info
         var album = { name: JSON.parse(body).name, imageUrl: JSON.parse(body).images[1].url }
+
+        //Creates an array with all the retrieved songs
         var songs = JSON.parse(body).tracks.items
 
+        //Concats the song's id into a single string
         var ids = "";
         songs.forEach((song) => {
             ids += song.id + ","
         })
 
-        // console.log(ids.replace(/,\s*$/, ""))
-
+        //Prepares a request to get the audio features of all the songs
         var options = {
             method: 'GET',
             url: 'https://api.spotify.com/v1/audio-features?ids=' + ids,
@@ -232,9 +240,13 @@ var getAlbums = (album, callback) => {
         request(options, function (error, response, body) {
             if (error) return callback(error);
 
+            //Creates and object with all the results
             var songsFeatures = JSON.parse(body);
 
+            //Checks if the results are valid
             if (songsFeatures.audio_features)
+
+                //Extracts the necessary info from each song
                 songsFeatures.audio_features.forEach((features, i) => {
                     var songData = {}
                     songData.name = songs[i].name;
@@ -259,6 +271,7 @@ var getAlbums = (album, callback) => {
 
 var getSaddestSongs = (song, callback) => {
 
+    //Prepare request to search for the song's lyrics
     var options = {
         method: 'GET',
         url: 'https://api.genius.com/search',
@@ -274,22 +287,36 @@ var getSaddestSongs = (song, callback) => {
 
         var lyricsArray = JSON.parse(body)
 
+        //If lyrics were retrieved
         if (lyricsArray.response.hits.length > 0) {
+
+            //Instnace of the normalized track title, artist name and the lyric's full title
             var nFullTitke = normalizeText(lyricsArray.response.hits[0].result.full_title)
             var nSongName = normalizeText(song.name)
             var nSongArtist = normalizeText(song.artist)
-            // console.log('%s contains \'%s\' and \'%s\'? %s', nFullTitke, nSongName, nSongArtist, nFullTitke.includes(nSongName))
+
+            //Checks if the lyric's indeed belong to the searched song
             if (nFullTitke.includes(nSongName)) {
+
+                //Use the Jsdom module to extract the lyrics from the lyric's URL
                 jsdom.env(
                     lyricsArray.response.hits[0].result.url,
                     ["http://code.jquery.com/jquery.js"],
                     (err, window) => {
+
+                        //Extracts the text from the 'lyrics' tag and removes the extra spaces, tabs and line jumps
                         song.lyrics = window.$("lyrics").text().replace(/\r?\n|\r/g, ' ').replace(/\[.*?\]/g, '').replace(/\s\s+/g, ' ')
+
+                        //Checks if there are still lyrics
                         if (song.lyrics && song.lyrics !== "" && song.lyrics !== " ") {
+
+                            //Rates the song
                             song = scoreLyrics(song)
                             return callback(null, song)
                         }
                         else {
+
+                            //Checks if even though there are no lyrics, the song should be rated 
                             if (factorNoLyrics === true) {
                                 song = scoreLyrics(song)
                                 return callback(null, song)
@@ -301,7 +328,8 @@ var getSaddestSongs = (song, callback) => {
                 );
             }
             else {
-                // song = scoreLyrics(song)
+
+                //Checks if even though there are no lyrics, the song should be rated
                 if (factorNoLyrics === true) {
                     song = scoreLyrics(song)
                     return callback(null, song)
@@ -311,7 +339,8 @@ var getSaddestSongs = (song, callback) => {
             }
         }
         else {
-            // song = scoreLyrics(song)
+            
+            //Checks if even though there are no lyrics, the song should be rated
             if (factorNoLyrics === true) {
                 song = scoreLyrics(song)
                 return callback(null, song)
@@ -323,9 +352,10 @@ var getSaddestSongs = (song, callback) => {
     });
 }
 
-var loadSadWords = (file) => {
+// Loads the words rated as 'sad' from the Lexicon into an array
+var loadSadWords = () => {
     var sadWords = [];
-    var lines = fs.readFileSync(file, 'ascii').split('\r\n');
+    var lines = fs.readFileSync('NRC-Emotion-Lexicon-Wordlevel-v0.92.txt', 'ascii').split('\r\n');
 
     lines.forEach((line) => {
         var [term, category, flag] = line.split('\t');
@@ -340,14 +370,17 @@ var loadSadWords = (file) => {
     return sadWords;
 }
 
-var sadWords = loadSadWords('NRC-Emotion-Lexicon-Wordlevel-v0.92.txt');
+var sadWords = loadSadWords();
 
+//Calculates a certain song's Gloom Index 
 var scoreLyrics = (song) => {
 
+    //Checks if the song has lyrics
     if (song.lyrics) {
+
+        //Creates a corpus with the every word from the lyrics using the Text Miner module
         var text = song.lyrics.replace(/\(|\)|:|\'/g, '');
         var corpus = new tm.Corpus([]);
-
         corpus.addDoc(text);
         corpus
             .clean()
@@ -358,53 +391,60 @@ var scoreLyrics = (song) => {
             .toLower()
             .stem('Lancaster');
 
+        //Parses the corpus into an array called vocabulary
         var terms = new tm.Terms(corpus);
         var vocabulary = terms.vocabulary;
+
+        //Counts how many words there are in the lyrics
         var counts = terms.dtm[0];
         var wordCount = counts.reduce((a, b) => a + b, 0);
+
         var sadWordCount = 0;
 
         vocabulary.forEach((word, i) => {
+            
+            //Checks if a word is in the sadWords array
             if (sadWords.indexOf(word) > -1) {
                 sadWordCount = sadWordCount + (1 * counts[i]);
             }
         });
 
         console.log('%s sad out of %s in %s, so thats a %s', sadWordCount, wordCount, song.name, (sadWordCount / wordCount))
-        song.pctSad = sadWordCount / wordCount
-        song.lyricalDensity = wordCount / song.duration
-        song.gloomIndex = ((1 - song.valence) + (song.pctSad * (1 + song.lyricalDensity))) / 2
+
+        //Caluclates the pecentage of sad words in the lyrics
+        var pctSad = sadWordCount / wordCount
+
+        //Calculated the lyrical density of the song
+        var lyricalDensity = wordCount / song.duration
+
+        //Calculates the song's Gloom Index
+        song.gloomIndex = ((1 - song.valence) + (pctSad * (1 + lyricalDensity))) / 2
         // console.log('Gloom index for lyrics %s: %2', song.name, song.gloomIndex)
+
+        delete song.lyrics;
+        delete song.duration; 
+        delete song.valence;
+        
         return song
     }
     else {
+        //if there where no lyrics, we asign an low arbitrary value to the necessary
+        //parameters to calculate the song's Gloom Index
         song.gloomIndex = ((1 - song.valence) + (0.001 * (1 + 0.001))) / 2
         console.log('Gloom index for non lyrics %s: %2', song.name, song.gloomIndex)
+        
         return song
     }
 }
 
+
+//Removes special characters from a string and turns it to lower case
 var normalizeText = (text) => {
     return text.toLowerCase().replace(/[^a-zA-Z0-9\s]/g, "")
 }
 
-var getQueryVariable = (variable) => {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
-        if (pair[0] == variable) { return pair[1]; }
-    }
-    return (false);
-}
 
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
